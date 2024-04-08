588017ba8b7e3fbd19e0105b17901498b3a79ba8
ssjh23 <seansoojh23@gmail.com>
2024-04-08 23:21:02 +0800

    feat: added check for duplicate student id

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /* Additional custom function files*/
 #include "parser.h"
 #include "eventTranslator.h"
@@ -14,6 +15,7 @@ int main(int argc, char **argv)
     char* studentID;
     timeLinkedList *list;
     int numberOfNodes;
+    int validStudentID;
     /* Check for valid file input */
     if (argc == 1)
     {
@@ -25,19 +27,43 @@ int main(int argc, char **argv)
     list = createLinkedList();
     for (i = 1; i < argc; i++)
     {
+        validStudentID = 0;
         studentID = (char *)malloc(sizeof(char) * 20);
         ics = parseFile(argv[i]);
 
-        printf("ICS Filename: %s\n", ics.filename);
-         /* Ask user for student name corresponding to ics file */
-        printf("Enter the ID of the student that uses this ics file: ");
+        do {
+            printf("ICS Filename: %s\n", ics.filename);
+            /* Ask user for student name corresponding to ics file */
+            printf("Enter the ID of the student that uses this ics file: ");
 
-        scanf("%s", studentID);
-        while (atoi(studentID) == 0)
-        {
-            printf("Please enter a valid student ID: ");
             scanf("%s", studentID);
-        }
+            if (atoi(studentID) == 0)
+            {
+                printf("Please enter a valid student ID: ");
+                continue;
+            }
+            if (i-1 > 0)
+            {
+                int foundMatchingID;
+                foundMatchingID = 0;
+                for (j = 0; j < i-1; j++)
+                {
+                    if (strcmp(studentID, studentIDS[j]) == 0)
+                    {
+                        printf("Student ID already exists. Please enter a different student ID: ");
+                        foundMatchingID = 1;
+                        validStudentID = 0;
+                        continue;
+                    }
+                }
+                if (foundMatchingID == 1) {
+                    continue;
+                }
+                validStudentID = 1;
+            } 
+            validStudentID = 1;
+        } while(!validStudentID);
+
 
         studentIDS[i - 1] = studentID;
         printf("No. Events: %d\n", ics.numEvents);
